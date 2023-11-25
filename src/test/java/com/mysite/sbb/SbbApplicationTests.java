f25ecb5823e4fb6c536b7da2bcdcaa9042ca2eaf
steve.no <jeaniedaddy@gmail.com>
2023-11-25 12:18:56 -0500

    add JPA Entity

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,29 @@
 package com.mysite.sbb;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SbbApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+	
 	@Test
-	void contextLoads() {
+	void testJpa() {
+		Optional<Question> oq = this.questionRepository.findById(1);
+		if(oq.isPresent()) {
+			Question q = oq.get();
+			System.out.println(q.getSubject());
+			assertEquals("what is sbb?",q.getSubject());
+		}
 	}
 
 }
